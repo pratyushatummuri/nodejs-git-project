27550a3dba3b1d15c87634f6248c95992d64a12b
pratyusha <pratyushatl@hotmail.com>
2021-05-29 00:29:31 +0530

    404 console log added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http=require("http");
 http
     .createServer((req,res) => {
-        console.log(res)
+        console.log(res);
+        console.log("404-page has been visited");
         res.write("<h1>Home Page!</h1>");
         res.end();
 
