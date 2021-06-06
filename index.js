89dfc772bfd18a5659fbbc100bf724ad258d0862
pratyusha <pratyushatl@hotmail.com>
2021-06-06 06:09:50 +0530

    nothing
    This reverts commit 42175ebeb2b5203937633c85a17114b8b7e08b6c.

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ http
         res.write("<h1>Home Page!</h1>");
         res.end();
 
+<<<<<<< HEAD
         //404- feature implemented
         //latest change
         
+=======
+        //nothing changes
+>>>>>>> parent of 42175eb (abort feature added)
     })
     .listen(8080);
